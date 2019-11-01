e7554ae6ad244f532da518f7b9fc7b36f9c254b2
Sam Forrester <SForrester@spartaglobal.com>
2019-11-01 09:01:16 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# mongo_jenkins
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: mongo_jenkins
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
